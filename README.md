adf4a94383172b8d45076c14caac27ea1a05a889
Silas Sefas <silassefas23@gmail.com>
2025-02-21 10:34:24 -0300

    Initial commit: Criando o projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Desafio-Avanade-DIO
---
+++ src/test/java/Desafio_Avanade_DIO/DesafioAvanadeDioApplicationTests.java
@@ -0,0 +1,13 @@
+package Desafio_Avanade_DIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAvanadeDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

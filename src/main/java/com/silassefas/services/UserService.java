41446c74a6152b4a40a6b6f0d44a01c495d2dc84
Silas Sefas <silassefas23@gmail.com>
2025-02-25 08:15:40 -0300

    Criando a camada de serviço

---
+++ src/main/java/com/silassefas/services/UserService.java
@@ -0,0 +1,11 @@
+package com.silassefas.services;
+
+import com.silassefas.domain.model.User;
+
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User user);
+
+}
---
+++ src/main/java/com/silassefas/services/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.silassefas.services.impl;
+
+import com.silassefas.domain.model.User;
+import com.silassefas.domain.repository.UserRepository;
+import com.silassefas.services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User user) {
+        if(userRepository.existsByAccountNumber(user.getAccount().getNumber())) {
+            throw new IllegalArgumentException("Account number already exists");
+        }
+
+        return userRepository.save(user);
+    }
+}
